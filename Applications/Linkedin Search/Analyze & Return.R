cf637eb5e569e9b4adaf2911daeea2ba63f17b25
rand0wn <abhishek18kushwah@gmail.com>
2015-12-08 01:22:57 +0530

    Added Search Function and Tested Results from Show

--- Applications/Linkedin Search/Analyze & Return.R
+++ Applications/Linkedin Search/Analyze & Return.R
@@ -6,30 +6,40 @@
 
 #1.Name 2.Location 3.Organisation 4.Role 5.Info
 
-Show <- data.frame()
 
+search <- function(result)
+{
+
+  Show <- data.frame()
+  
 if(Name != "") #Name is not empty
 {
-  Show <- subset(result, grepl(Name, result$Name, fixed = TRUE, ignore.case = TRUE ))
+  Show <- subset(result, mapply(grepl, Name, result$Name))
 }  
 
 if(Location != "") #Location is not empty
 {  
-  Show <- subset(Show, grepl(Location, result$Location ))
+  Show <- subset(Show, mapply(grepl, Location, result$Location))
 }
 
 if(Organisation != "") #Organisation is not empty
 {
-  Show <- subset(Show, grepl(Organisation, result$Organisation )) 
+  Show <- subset(Show, mapply(grepl, Organisation, result$Organisation)) 
 }
 
 if(Role != "") #Role is not empty
 {
-  Show <- subset(Show, grepl(Role, result$Role ))
+  Show <- subset(Show, mapply(grepl, Role, result$Role))
 }
 
 if(Info != "") #Info is not empty
 {
-  Show <- subset(Show, grepl(Info, result$Info )) 
+  Show <- subset(Show, mapply(grepl, Info, result$Info)) 
 }
 
+  return(Show)
+}    
+
+Show <- search(result)
+
+View(Show)
